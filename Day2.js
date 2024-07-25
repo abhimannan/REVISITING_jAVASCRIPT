606ddc429aa2344317d65feeaf3508fa77a7fa1f
K Abhi <abhi543it@gmail.com>
2024-07-25 21:14:07 +0530

    Update Day2.js

--- Day2.js
+++ Day2.js
@@ -84,3 +84,117 @@ if (year % 4 === 0) {
 } else {
  console.log(`${year} is not leap year`);
 }
+
+// Arrays
+let names=["abhi","geetha","kumar","vijay"];
+// Accessing array elements using their index
+   console.log(names[2]);
+
+let requiredname=window.prompt("Enter the name u required : ");
+if(requiredname==="mother"){
+  console.log(names[1]);
+}
+else if(requiredname==="father"){
+  console.log(names[2]);
+}
+else if(requiredname==="brother"){
+  console.log(names[3]);
+}
+else if(requiredname==="myname"){
+  console.log(names[0]);
+}
+
+// Task :
+
+let required=window.prompt("Enter the name to be chech : ");
+if(names.includes(required)){
+  console.log(`${required} Available in the array`);
+}
+else{
+  console.log(`${required} Doesn't exists in the array`);
+}
+
+// Task : Write a pragram thats the numbers from 1 to 100.But for multiples of three print 
+//        "Fizz" instead of the number and for the multiples of five print "Buzz".For numbers
+//         which are multiples of both three and five print "FizzBuzz";
+
+//printing number sfrom 1 to 100
+for(let i=1;i<=100;i++){
+    // if i divisible by 3
+    if(i%3===0){
+          console.log("Fizz");
+    }  
+      // if i divisible by 5
+      else if(i%5===0){
+           console.log("Buzz");
+       }
+       else if(i%3===0 && i%5===0){
+          console.log(i);
+       }
+       // if i is not divisible by 3,5   
+       if(i%3===0 && i%5===0){
+            console.log("FizzBuss");
+       }
+      else if(i%3!==0 && i%5!==0){
+           console.log(i);
+       }
+}
+
+// Another Method To Solve The Above Task   ==> Using WhileLoop
+var arr=[];    // take an empty array
+var count=0;   // take the temporary count variable
+function add(){   // function
+  while(count<=100){  // while loop is continuously executed untill the condion is false
+  
+     if(count%3===0){
+       arr.push("Fizz");   // insert an value into the arr  []
+     }
+     else if(count%5==0){
+       arr.push("Buzz");
+     }
+     else if(count%3==0 && count%5==0){ // if the both the conditions satisfied
+       arr.push("FizzBuzz");
+     }
+     else{
+        arr.push(count);   // if nothing matches then just print the count value
+    }  
+      count++;  // increment the count value by 1 after every interation
+     }
+     console.log(arr);   // print the array
+   }
+add();   // calling the function
+
+// Solving the above problem using ForLoop
+// The problem in the while loop is it may be turn into the "INFINITE LOOP"
+var arr=[];    // take an empty array 
+function add(){   // function
+  for(let count=1;count<=100;count++){  // for loop
+  
+     if(count%3===0){
+       arr.push("Fizz");   // insert an value into the arr  []
+     }
+     else if(count%5==0){
+       arr.push("Buzz");
+     }
+     else if(count%3==0 && count%5==0){ // if the both the conditions satisfied
+       arr.push("FizzBuzz");
+     }
+     else{
+        arr.push(count);   // if nothing matches then just print the count value
+    }  
+     }
+     console.log(arr);   // print the array
+   }
+  
+add();   // calling the function
+
+// THIS CODE I'M CAN'T ABEL TO SOLVE IN FUTUTE I WILL SOLVE
+// var bottles=99;
+// function display(){
+//   while(bottles>=0){
+//       console.log(`${bottles} of the beer on the wall,${bottles} bottles of beer.Take one down and pass it.${bottles-1} bottles of beer on the wall`);
+//   }
+//   bottles--;
+
+// }
+// display();
