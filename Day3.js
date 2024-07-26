ecf23445dabf5b7e2c83c5b4912f6d8dbad89dc6
K Abhi <abhi543it@gmail.com>
2024-07-26 21:03:51 +0530

    Update Day3.js

--- Day3.js
+++ Day3.js
@@ -20,3 +20,55 @@ for(let i=n1;i<=n;i++){
 }
 fib(n);
 
+
+// DOM
+// Accesing the css
+function submit(){
+ let x=document.getElementById("abhi");
+ x.innerHTML="ABHI";
+ x.style.color="green";
+ x.style.margin="50px";
+ x.style.width="150px";
+ x.style.height="150px";
+ x.style.backgroundColor="red";
+}
+submit();
+
+var x= document.getElementById("abhi");
+   document.getElementById("click").onclick(()=>{
+        x.textContent="ABHI";
+   });
+
+// DOM is an object
+// Accessing the tags of the html document like 
+//                                 head,title,body,button,...
+
+console.log(document);
+document.title="title accessing";
+document.body.style.backgroundColor="yellow";
+
+// selecting the tags using tagnames
+let x=document.getElementsByTagName("h1");
+console.log(x);
+// selecting the tags using id
+let id=document.getElementById("abhi");
+console.log(id);
+// by class name
+var class_name=document.getElementsByClassName("btn");
+console.log(class_name);
+// // QuerySelector
+let queryselect=document.querySelector("#abhi");
+console.log(queryselect);
+
+// Events onclick(function),onmouseover(function),onmouseout();
+let btn=document.querySelector("#abhi");
+function submit(){
+   btn.style.color="red";
+}
+function change(){
+   btn.style.backgroundColor="yellow";
+}
+function over(){
+   document.body.style.backgroundColor="green";
+}
+
